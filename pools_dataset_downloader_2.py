dd1c37fcfe9b11a40a6bf31b03c9d0f395efa7ff
Yuri Turygin <yuri.turygin@gmail.com>
2020-12-17 09:19:20 -0500

    added spyder project files

--- pools_dataset_downloader_2.py
+++ pools_dataset_downloader_2.py
@@ -5,9 +5,6 @@ import xlwings as xw
 import datetime as dt
 import time
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------------------------------------------------------------------
-
 def runKDSQuery(spreadsheet):
     if os.path.exists(spreadsheet):
         xl = win32com.client.DispatchEx("Excel.Application")
@@ -42,90 +39,76 @@ def pickupResults(spreadsheet,outCSV='out.csv',mode='w',outputSheet="Output",res
         else:
             print("unknown to_csv writing mode: " + mode)            
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------
 # Type=PoolByPool,x=poolnumber, NonBlank=yes/y=cusip: prefix: spread: cpr1: cpr3: cpr6: cpr12: smm: DayCount: origb: currb: prevb: paydown: Prepay: factor: ocoupon: coupon: owac: wac: wam: age: aols: waols: cwals: owals: clnsz: olnsz: onloans: cnloans: pcnloans: ppnloans: osato: csato: oltv: cltv: FnBrk_OCLTV: FnBrk_CCLTV: fico: ODTI: CODTI: %CashWindow: %Majors: PurpPctpurchase: PurpPctrefi: ChannelPctBroker: ChannelPctCorr: ChannelPctRetail: OccPctowner: OccPct2ndHome: OccPctinvestor: PropUnitsPct2-4: Burnout: wac_min: wac_qtr1: wac_qtr3: wac_max: ofico_min: ofico_qtr1: ofico_qtr3: ofico_max: oltv_min: oltv_qtr1: oltv_qtr3: oltv_max: lnsz_min: lnsz_qtr1: lnsz_qtr3: lnsz_max: hpa3m: hpa1: hpa5: hpaLife: hpaPO3m: hpaPO1: hpaPO5: hpaPOLife, Agency=fn, MortgageType=fix, Program=30, DateWindowPeriod=202011, Issuance=201001: 202012,  poolType=bottom,
 def poolAttributesQuery(observationWindow,issueWindow,program='30'):
     print(observationWindow + ' | ' + issueWindow)
     return "Type=PoolByPool,x=poolnumber, NonBlank=yes/y=cusip: prefix: spread: cpr1: cpr3: cpr6: cpr12: smm: DayCount: origb: currb: prevb: paydown: Prepay: factor: ocoupon: coupon: owac: wac: wam: age: aols: waols: cwals: owals: clnsz: olnsz: onloans: cnloans: pcnloans: ppnloans: osato: csato: oltv: cltv: FnBrk_OCLTV: FnBrk_CCLTV: fico: ODTI: CODTI: %CashWindow: %Majors: PurpPctpurchase: PurpPctrefi: ChannelPctBroker: ChannelPctCorr: ChannelPctRetail: OccPctowner: OccPct2ndHome: OccPctinvestor: PropUnitsPct2-4: Burnout: wac_min: wac_qtr1: wac_qtr3: wac_max: ofico_min: ofico_qtr1: ofico_qtr3: ofico_max: oltv_min: oltv_qtr1: oltv_qtr3: oltv_max: lnsz_min: lnsz_qtr1: lnsz_qtr3: lnsz_max: hpa3m: hpa1: hpa5: hpaLife: hpaPO3m: hpaPO1: hpaPO5: hpaPOLife, Agency=fn, MortgageType=fix, Program=" + program + ", DateWindowPeriod=" + observationWindow + ", Issuance=" + issueWindow + ", poolType=bottom,"
-#-------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Type=PoolByPool,x=poolnumber, NonBlank=yes/y=cusip: StatePoolPctAK: StatePoolPctAL: StatePoolPctAR: StatePoolPctAZ: StatePoolPctCA: StatePoolPctCO: StatePoolPctCT: StatePoolPctDC: StatePoolPctDE: StatePoolPctFL: StatePoolPctGA: StatePoolPctGU: StatePoolPctHI: StatePoolPctIA: StatePoolPctID: StatePoolPctIL: StatePoolPctIN: StatePoolPctKS: StatePoolPctKY: StatePoolPctLA: StatePoolPctMA: StatePoolPctMD: StatePoolPctME: StatePoolPctMI: StatePoolPctMN: StatePoolPctMO: StatePoolPctMS: StatePoolPctMT: StatePoolPctNC: StatePoolPctND: StatePoolPctNE: StatePoolPctNH: StatePoolPctNJ: StatePoolPctNM: StatePoolPctNV: StatePoolPctNY: StatePoolPctOH: StatePoolPctOK: StatePoolPctOR: StatePoolPctPA: StatePoolPctPR: StatePoolPctRI: StatePoolPctSC: StatePoolPctSD: StatePoolPctTN: StatePoolPctTX: StatePoolPctUT: StatePoolPctVA: StatePoolPctVI: StatePoolPctVT: StatePoolPctWA: StatePoolPctWI: StatePoolPctWV: StatePoolPctWY, Agency=fn, MortgageType=fix, Program=30, DateWindowPeriod=201001: current, Issuance=201001, poolType=bottom,
 def geoPctQuery(observationWindow,issueWindow,program='30'):
     print(observationWindow + ' | ' + issueWindow)
     return "Type=PoolByPool,x=poolnumber, NonBlank=yes/y=cusip: StatePoolPctAK: StatePoolPctAL: StatePoolPctAR: StatePoolPctAZ: StatePoolPctCA: StatePoolPctCO: StatePoolPctCT: StatePoolPctDC: StatePoolPctDE: StatePoolPctFL: StatePoolPctGA: StatePoolPctGU: StatePoolPctHI: StatePoolPctIA: StatePoolPctID: StatePoolPctIL: StatePoolPctIN: StatePoolPctKS: StatePoolPctKY: StatePoolPctLA: StatePoolPctMA: StatePoolPctMD: StatePoolPctME: StatePoolPctMI: StatePoolPctMN: StatePoolPctMO: StatePoolPctMS: StatePoolPctMT: StatePoolPctNC: StatePoolPctND: StatePoolPctNE: StatePoolPctNH: StatePoolPctNJ: StatePoolPctNM: StatePoolPctNV: StatePoolPctNY: StatePoolPctOH: StatePoolPctOK: StatePoolPctOR: StatePoolPctPA: StatePoolPctPR: StatePoolPctRI: StatePoolPctSC: StatePoolPctSD: StatePoolPctTN: StatePoolPctTX: StatePoolPctUT: StatePoolPctVA: StatePoolPctVI: StatePoolPctVT: StatePoolPctWA: StatePoolPctWI: StatePoolPctWV: StatePoolPctWY, Agency=fn, MortgageType=fix, Program=" + program + ", DateWindowPeriod=" + observationWindow + ", Issuance=" + issueWindow + ", poolType=bottom,"
-#-------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Type=PoolByPool,x=poolnumber, NonBlank=yes/y=SellerPctAMRHT: SellerPctALS: SellerPctCAFULL: SellerPctCNTL: SellerPctCITIZ: SellerPct53: SellerPctFIR: SellerPctFRDOM: SellerPctGUILD: SellerPctCHASE: SellerPctLLSL: SellerPctMATRX: SellerPctNCM: SellerPctNATIONSTAR: SellerPctNRESM: SellerPctPNYMAC: SellerPctPILOSI: SellerPctQUICK: SellerPctREG: SellerPctRMSC: SellerPctUNSHFI: SellerPctWFHM: cusip: prefix, Agency=fn, MortgageType=fix, Program=30, DateWindowPeriod=current, Issuance=202001,  poolType=bottom,
 def poolSellerPctQuery(observationWindow,issueWindow,program='30'):
     print(observationWindow + ' | ' + issueWindow)
     return "Type=PoolByPool,x=poolnumber, NonBlank=yes/y=SellerPctAMRHT: SellerPctALS: SellerPctCAFULL: SellerPctCNTL: SellerPctCITIZ: SellerPct53: SellerPctFIR: SellerPctFRDOM: SellerPctGUILD: SellerPctCHASE: SellerPctLLSL: SellerPctMATRX: SellerPctNCM: SellerPctNATIONSTAR: SellerPctNRESM: SellerPctPNYMAC: SellerPctPILOSI: SellerPctQUICK: SellerPctREG: SellerPctRMSC: SellerPctUNSHFI: SellerPctWFHM: cusip: prefix, Agency=fn, MortgageType=fix, Program=" + program + ", DateWindowPeriod=" + observationWindow + ", Issuance=" + issueWindow + ", poolType=bottom,"
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------    
-
-def fullQueryRun(queryBuilder,observationWindows,issueWindows,outCSV,program='30'):
+def fullQueryRun(queryBuilder,query_running_spreadsheet,observation_window,issue_window,output_csv,program='30'):
 
     starttime = time.perf_counter()
     
-    if os.path.exists(outCSV):
-        print("Removing ",outCSV)
-        os.remove(outCSV)
+    if os.path.exists(output_csv):
+        print("Removing ",output_csv)
+        os.remove(output_csv)
         
-    for observation_window in observationWindows:
-        for issue_window in issueWindows:
-            print('--------------------------------------------------------------------------------------')
-            print(dt.datetime.now().strftime("%A, %B %d, %Y, %I:%M%p"))
-        
-            query = queryBuilder(observation_window,issue_window,program)
-            print(query)
-            
-            editQuery(query,spreadsheet);
-            print('                    :  query edited, running')
-            runKDSQuery(spreadsheet)
-            print('                    :  query finished, collecting')
-            pickupResults(spreadsheet,outCSV,mode='a',resultsLabel="issue="+issue_window+" | observ="+observation_window)
-            print('                    :  query results appended')
-            print('--------------------------------------------------------------------------------------')
+    print('--------------------------------------------------------------------------------------')
+    print(dt.datetime.now().strftime("%A, %B %d, %Y, %I:%M%p"))
+
+    query = queryBuilder(observation_window,issue_window,program)
+    print(query)
+    
+    editQuery(query,query_running_spreadsheet);
+    print('                    :  query edited, running')
+    runKDSQuery(query_running_spreadsheet)
+    print('                    :  query finished, collecting')
+    pickupResults(query_running_spreadsheet,output_csv,resultsLabel="issue="+issue_window+" | observ="+observation_window)
+    print('                    :  query results appended')
+    print('--------------------------------------------------------------------------------------')
     
     endtime = time.perf_counter()
     print('elapsed time        : ',"%1.1f" % (endtime - starttime),'sec /',"%1.2f" % ((endtime - starttime)/60),'min')
     print('--------------------------------------------------------------------------------------')
 
-#------------------------------------------------------------------------------------------------------
-    
-#------------------------------------------------------------------------------------------------------
-#---------------------------------- MAIN --------------------------------------------------------------
-#------------------------------------------------------------------------------------------------------    
     
 if __name__ == "__main__":
     
-    spreadsheet = "./KDS/kds_macro.xlsm"
+    query_running_spreadsheet = "./KDS/kds_macro.xlsm"
     data_dir    = "../data by factor date"
     
     todaysDate = dt.datetime.now(); 
         
-    issue_periods = list(pd.Series(pd.date_range('2010-01-01',todaysDate,freq='M'),name='Issue Dates').apply(lambda x: x.strftime('%Y%m')))
+    issue_periods = ['201001:202012']
     
-    observation_periods = list(map(lambda x: str(x) + '01: ' + str(x) + '12',range(2010,2021,1)))
-#    observation_periods = ['202002:202010']
-#    observation_periods.append('current: current')
+    observation_periods = ['202011']
+    
+    observation_periods = list()
+    for y in range(2010,2021):
+        for m in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+            observation_periods.append(str(y) + str(m))
+    observation_periods.pop()
+    
 
     #------------------------------------------------------------------------------------------------------   
-    program = 'jumbo30'
+    #program = 'jumbo30'
+    program = '30'
+    #------------------------------------------------------------------------------------------------------   
     # download pool attributes data
-    for i in range(len(issue_periods)):
-        if i<1e6:
-            issPeriod = issue_periods[i]
-            year_of_issue = issPeriod[0:4]
-            this_observ_periods = list()
-            for ob_period in observation_periods:
-                ob_period_start = ob_period.split(":")[0]
-                if ob_period_start != 'current':
-                    ob_period_year = ob_period_start[0:4]
-                    if int(year_of_issue) <= int(ob_period_year):
-                        this_observ_periods.append(ob_period)
-            #this_observ_periods.append(observation_periods[-1])
-            #------------------------------------------------------------------------------------------------------
-#            print(f'{this_observ_periods} | {issPeriod}')
-            fullQueryRun(poolAttributesQuery,this_observ_periods,issPeriod,data_dir + "/pools attributes/pools_attributes_issued_" + issPeriod + ".csv",program)
-            #------------------------------------------------------------------------------------------------------
+    for observation_window in observation_periods:
+        for issue_window in issue_periods:
+            fullQueryRun(poolAttributesQuery,query_running_spreadsheet,observation_window,issue_window,data_dir + "/pools attributes/pools_attributes_by_fct_date_" + observation_window + ".csv",program)
+    #------------------------------------------------------------------------------------------------------   
+
     #------------------------------------------------------------------------------------------------------  
     # download pool geographical data
     for i in range(len(issue_periods)):
